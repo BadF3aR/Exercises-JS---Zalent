f1523d84fd9fc2a31cde37100bb04c0e9cb94e14
BadF3aR <cziken99@gmail.com>
2022-04-05 14:05:48 +0200

    update 14;05

--- exercise1-3.js
+++ exercise1-3.js
@@ -1,10 +1,23 @@
 
 var numer = Math.floor(Math.random()*5)+1;
+var timer1 = 0;
+var timer2 = 0;
+
+function ustawslajd(nrslajdu)
+{
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    numer = nrslajdu -1;
+
+    schowaj();
+    setTimeout("zmienslajd()",500);
+}
 
 function schowaj()
 {
     $("#slider").fadeOut(500);
 }
+
 function zmienslajd()
 {
     numer++;
@@ -15,6 +28,6 @@ function zmienslajd()
     document.getElementById("slider").innerHTML = plik;
     $("#slider").fadeIn(500);
 
-    setTimeout("zmienslajd()", 5000);
-    setTimeout("schowaj()", 4500);
+    timer1 = setTimeout("zmienslajd()", 5000);
+    timer2 = setTimeout("schowaj()", 4500);
 }
