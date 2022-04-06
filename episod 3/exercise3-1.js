94f150e3a0d2318b857451faf2c6bafe4a407021
BadF3aR <cziken99@gmail.com>
2022-04-06 17:17:43 +0200

    update 17;17

--- episod 3/exercise3-1.js
+++ episod 3/exercise3-1.js
@@ -2,7 +2,7 @@ var haslo = "Bez pracy nie ma kołaczy";
 haslo = haslo.toUpperCase();
 
 var dlugosc = haslo.length; 
-
+var ile_skuch = 0;
 var haslo1 = "";
 
 for (i=0; i<dlugosc; i++)
@@ -63,10 +63,62 @@ function start()
     for(i=0; i<=34; i++)
     {
         var element = "lit" + i;
-        tresc_diva = tresc_diva + '<div class="litera" id="'+ element +'">'+ litery[i] +'</div>';
+        tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz('+i+')" id="'+ element +'">'+ litery[i] +'</div>';
     }
 
     document.getElementById("litery").innerHTML = tresc_diva;
 
     wypisz_haslo();
+}
+
+String.prototype.ustawZnak = function(miejsce, znak)
+{
+    if(miejsce > this.length -1) return this.toString();
+    else return this.substr(0,miejsce) + znak + this.substr(miejsce+1);
+}
+
+function sprawdz(nr)
+{
+    var trafiona = false;
+
+    for(i=0; i<dlugosc; i++)
+    {
+        if(haslo.charAt(i) == litery[nr])
+        {
+            haslo1 = haslo1.ustawZnak(i,litery[nr]);
+            trafiona = true;
+        }
+    }
+
+    if(trafiona == true) // == operator funkcji
+    {
+        var element = "lit" + nr;
+        document.getElementById(element).style.background = "#003300";
+        document.getElementById(element).style.color = "#00C000";
+        document.getElementById(element).style.border = "2px solid #00C000";
+        document.getElementById(element).style.cursor = "default";
+
+        wypisz_haslo();
+    }
+    else
+    {
+        var element = "lit" + nr;
+        document.getElementById(element).style.background = "#330000";
+        document.getElementById(element).style.color = "#C00000";
+        document.getElementById(element).style.border = "2px solid #C00000";
+        document.getElementById(element).style.cursor = "default";
+        document.getElementById(element).setAttribute("onclick",";");
+
+        //skucha
+        ile_skuch++;
+        var obraz = "img/s"+ ile_skuch + ".jpg";
+        document.getElementById("obrazek").innerHTML = '<img src="'+obraz+'" alt ="" />'; 
+    }
+    //wygrana
+    if(haslo == haslo1)
+    document.getElementById("litery").innerHTML = "Gratulacje! odgadłeś hasło!" + '<br/><br/><span class="rest" onclick="location.reload()">JESZCZE RAZ?</span>'
+    //przegrana
+    if(ile_skuch>=9)
+    document.getElementById("litery").innerHTML = "Przegrana!" + '<br/><br/><span class="rest" onclick="location.reload()">JESZCZE RAZ?</span>'
+
 }
